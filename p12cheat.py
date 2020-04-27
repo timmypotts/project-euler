b6a9e01e2ef1f381ba113746ecd21087c37b5f69
tim <timothy.potts@colorado.edu>
2020-04-27 17:13:42 -0600

    p10 comments

--- p12cheat.py
+++ p12cheat.py
@@ -1,7 +1,9 @@
 import time
 
+
 def num_divisors(n):
-    if n % 2 == 0: n = n/2
+    if n % 2 == 0:
+        n = n/2
     divisors = 1
     count = 0
     while n % 2 == 0:
@@ -18,6 +20,7 @@ def num_divisors(n):
         p += 2
     return divisors
 
+
 def find_triangular_index(factor_limit):
     n = 1
     lnum, rnum = num_divisors(n), num_divisors(n+1)
@@ -26,9 +29,10 @@ def find_triangular_index(factor_limit):
         lnum, rnum = rnum, num_divisors(n+1)
     return n
 
+
 start = time.time()
 index = find_triangular_index(500)
 triangle = (index * (index + 1)) / 2
 elapsed = (time.time() - start)
 
-print "result %s returned in %s seconds." % (triangle,elapsed)
+print("result %s returned in %s seconds." % (triangle, elapsed))
--- problem11.py
+++ problem11.py
@@ -16,36 +16,36 @@ for i in array:
     newArray.append(k)
 
 a = matrix(newArray)
-print a
+print(a)
 
 max = 0
 
-#row products
-for i in range(0,20):
-    for j in range(0,17):
-        pro = a[i,j]*a[i,j+1]*a[i,j+2]*a[i,j+3]
+# row products
+for i in range(0, 20):
+    for j in range(0, 17):
+        pro = a[i, j]*a[i, j+1]*a[i, j+2]*a[i, j+3]
         if(pro >= max):
             max = pro
 
-#collumn products
-for j in range(0,20):
-    for i in range(0,17):
-        pro = a[i,j]*a[i+1,j]*a[i+2,j]*a[i+3,j]
+# collumn products
+for j in range(0, 20):
+    for i in range(0, 17):
+        pro = a[i, j]*a[i+1, j]*a[i+2, j]*a[i+3, j]
         if(pro >= max):
             max = pro
 
-#downward diagonal products
-for i in range(0,17):
-    for j in range(0,17):
-        pro = a[i,j]*a[i+1,j+1]*a[i+2,j+2]*a[i+3,j+3]
-        if(pro>=max):
-            max=pro
+# downward diagonal products
+for i in range(0, 17):
+    for j in range(0, 17):
+        pro = a[i, j]*a[i+1, j+1]*a[i+2, j+2]*a[i+3, j+3]
+        if(pro >= max):
+            max = pro
 
-#upward diagonal products
-for i in range(3,20):
-    for j in range(0,17):
-        pro = a[i,j]*a[i-1,j+1]*a[i-2,j+2]*a[i-3,j+3]
-        if(pro>=max):
-            max=pro
+# upward diagonal products
+for i in range(3, 20):
+    for j in range(0, 17):
+        pro = a[i, j]*a[i-1, j+1]*a[i-2, j+2]*a[i-3, j+3]
+        if(pro >= max):
+            max = pro
 
-print ('The greatest product of four adjacent numbers in the above matrix is: %d' %(max))
+print('The greatest product of four adjacent numbers in the above matrix is: %d' % (max))
