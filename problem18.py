0b0b4f8bb7e1887a9ac7807f90d26a00cb26770f
Tim <timothy.potts@colorado.edu>
2020-10-20 15:16:38 -0600

    init p 18

---
+++ problem18.py
@@ -0,0 +1,40 @@
+# By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+#     3
+#    7 4
+#   2 4 6
+#  8 5 9 3
+
+# Find the maximum total from top to bottom of the triangle below:
+
+    #                     75
+    #                   95 64
+    #                 17 47 82
+    #                18 35 87 10
+    #               20 04 82 47 65
+    #              19 01 23 75 03 34
+    #             88 02 77 73 07 63 67
+    #           99 65 04 28 06 16 70 92
+    #         41 41 26 56 83 40 80 70 33
+    #        41 48 72 33 47 32 37 16 94 29
+    #       53 71 44 65 25 43 91 52 97 51 14
+    #     70 11 33 28 77 73 17 78 39 68 17 57
+    #    91 71 52 38 17 14 91 43 58 50 27 29 48
+    #   63 66 04 68 89 53 67 30 73 16 69 87 40 31
+    # 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+
+
+# Easier to think about when you realize the layout is this,
+#   V ---- x ---- x ---- D
+#   |      |      |
+#   x ---- x ---- C
+#   |      |
+#   x ---- B
+#   |
+#   A
+
+# I realized that this is just a directional graph, and the problem could be solved by slightly modifying an algorithm that finds the max number of paths from (A,B,C,D) to V.
+# I chose to use dynamic programming to speed up the process.
+
+
+pretree = [75, 95, 64, 17, 47, 82, 18, 35, 87, 10, 20, 04, 82, 47, 65, 19, 01, 23, 75, 03, 34, 88, 02, 77, 73, 07, 63, 67, 99, 65, 04, 28, 06, 16, 70, 92, 41, 41, 26, 56, 83, 40, 80, 70, 33, 41, 48, 72, 33, 47, 32, 37, 16, 94, 29, 53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14, 70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57, 91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48, 63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31, 04, 62, 98, 27, 23, 09, 70, 98, 73, 93, 38, 53, 60, 04, 23 ]
+
